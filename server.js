7c00d072795d8a6361dca7a9a595bfeab40178e8
ezzy03t4 <ezzy03t4@gmail.com>
2026-08-19 23:03:27 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5500',
   'http://localhost:5500',
   'http://localhost:5000',
+  'https://nexus.pxxlspace.cv',
   process.env.FRONTEND_URL
 ].filter(Boolean); // remove undefined
 
